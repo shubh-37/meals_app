3d84b677fb4cc7d13b8734fa6e456e755b76e430
Shubh Arya <shubh.arya37@gmail.com>
2024-05-27 21:19:50 +0530

    added meals screen

---
+++ lib/screens/meals.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget showMeals;
+    showMeals =
+        ListView.builder(itemBuilder: (ctx, index) => Text(meals[index].title));
+    if (meals.isEmpty) {
+      showMeals = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uhoh, nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Try selecting any other category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Go to Home'))
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: showMeals);
+  }
+}
