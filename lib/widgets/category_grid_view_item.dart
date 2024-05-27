8707d29d9282278bbb4d7b1d1394b2e46a8b4843
Shubh Arya <shubh.arya37@gmail.com>
2024-05-27 21:16:55 +0530

    added border radius and made the widget tappable

--- lib/widgets/category_grid_view_item.dart
+++ lib/widgets/category_grid_view_item.dart
@@ -7,19 +7,25 @@ class CategoryGridViewItem extends StatelessWidget {
   final Category category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          category.color.withOpacity(0.3),
-          category.color.withOpacity(0.9)
-        ], begin: Alignment.center, end: Alignment.topRight),
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.3),
+            category.color.withOpacity(0.9)
+          ], begin: Alignment.topLeft, end: Alignment.topRight),
+        ),
+        child: Text(category.title,
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground)),
       ),
-      child: Text(category.title,
-          style: Theme.of(context)
-              .textTheme
-              .titleLarge!
-              .copyWith(color: Theme.of(context).colorScheme.onBackground)),
     );
   }
 }
