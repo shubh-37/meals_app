743b6a5dfd1ee1f10d972c52efdeb8e09610c554
Shubh Arya <shubh.arya37@gmail.com>
2024-05-27 22:03:10 +0530

    added a function to change screen to meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_view_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) =>
+                MealsScreen(title: category.title, meals: filteredMeals)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +32,11 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 20),
         children: [
           for (var category in availableCategories)
-            CategoryGridViewItem(category: category)
+            CategoryGridViewItem(
+                category: category,
+                selectCategory: () {
+                  _selectCategory(context, category);
+                })
         ],
       ),
     );
