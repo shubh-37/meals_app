52f46f71996dbfdc80242142f7a253e852fb5631
Shubh Arya <shubh.arya37@gmail.com>
2024-05-27 22:02:49 +0530

    added item count of meals

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -10,8 +10,9 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget showMeals;
-    showMeals =
-        ListView.builder(itemBuilder: (ctx, index) => Text(meals[index].title));
+    showMeals = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title));
     if (meals.isEmpty) {
       showMeals = Center(
         child: Column(
