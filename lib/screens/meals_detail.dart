5b7a4503ec2a25e3f3aeb01ee5a749db9714524f
Shubh Arya <shubh.arya37@gmail.com>
2024-06-02 16:29:45 +0530

    added a new screen to show all the meal info

---
+++ lib/screens/meals_detail.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsDetailScreen extends StatelessWidget {
+  const MealsDetailScreen({super.key, required this.meal});
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(meal.title)),
+      body: Image.network(
+        meal.imageUrl,
+        height: 200,
+        width: double.infinity,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
